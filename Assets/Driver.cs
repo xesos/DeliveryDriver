11fc3153de98ce14c22a7047eee6bf6e1809f585
xesos <jesus.gianzo@gmail.com>
2024-06-15 16:04:02 +0100

    Add a car with user input movement

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] float steerSpeed = 300f;
+    [SerializeField] float moveSpeed = 20f;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;        
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        transform.Rotate(0,0,-steerAmount);
+        transform.Translate(0,moveAmount,0);
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # DeliveryDriver
- Primer practita Unity
+First game done using the Udemy course Learn Unity in C# & Code Your First Five 2D Video Games for Web, Mac & PC. The Tutorials Cover Tilemap
